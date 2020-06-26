369cb6333fe17a20f1adaadde81b8f13a5931b59
ArthurRmd <arthur.remond@protonmail.com>
2020-06-26 23:35:05 +0200

    add : page user

--- src/Controller/StatistiqueLolController.php
+++ src/Controller/StatistiqueLolController.php
@@ -4,9 +4,9 @@ namespace App\Controller;
 
 use App\LeagueOfLegendApi;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class StatistiqueLolController extends AbstractController
 {
@@ -66,6 +66,16 @@ class StatistiqueLolController extends AbstractController
 
 
 
+    }
+
+
+    /**
+     * @Route("/user", name="user")
+     * @return Response
+     */
+    public function user()
+    {
+        return $this->render('statistique/user.html.twig');
     }
 
 
