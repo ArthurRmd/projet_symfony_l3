33ecb4e9aa9ac65a0dfd229fd8894d91724f5311
ArthurRmd <arthur.remond@protonmail.com>
2020-06-28 22:33:48 +0200

    fix : api

--- src/LeagueOfLegendApi.php
+++ src/LeagueOfLegendApi.php
@@ -12,6 +12,39 @@ class LeagueOfLegendApi
 
     private static $apiKey = null;
 
+
+    /***********************************
+     *         Private function
+     **********************************/
+
+    private static function callApi(string $url, string $method = "GET")
+    {
+
+        $client = HttpClient::create();
+        $response = $client->request($method, $url);
+
+        return $response;
+
+    }
+
+    private static function setUpApiKey()
+    {
+        if (self::$apiKey == null && !empty($_ENV['API_KEY'])) {
+            self::$apiKey = $_ENV['API_KEY'];
+        } else if(self::$apiKey == null && empty($_ENV['API_KEY'])) {
+            throw new Exception('Error, you did not enter your Api Key in the env file');
+        }
+    }
+
+
+    /***********************************
+     *         Public function
+     **********************************/
+
+    /**
+     * @param int $id
+     * @return array|null
+     */
     public static function getChampion(int $id)
     {
 
@@ -30,31 +63,24 @@ class LeagueOfLegendApi
 
     public static function getAllChampions()
     {
+        self::setUpApiKey();
         $response = self::callApi('http://ddragon.leagueoflegends.com/cdn/10.10.3208608/data/en_US/champion.json');
 
         return $response->getStatusCode() === 200 ? $response->toArray()['data'] : null;
 
     }
 
-    private static function callApi(string $url, string $method = "GET")
-    {
 
-        self::setUpApiKey();
-        $client = HttpClient::create();
-        $response = $client->request($method, $url);
-
-        return $response;
-
-    }
 
     public static function getChampionRotation()
     {
-        $freeChampions = [];
-
-        $freeChampionsId = self::callApi('https://euw1.api.riotgames.com/lol/platform/v3/champion-rotations' . '?api_key=' . self::$apiKey)
+        self::setUpApiKey();
+        $freeChampionsId = self::callApi('https://euw1.api.riotgames.com/lol/platform/v3/champion-rotations?api_key=' . self::$apiKey)
             ->toArray();
 
         $champions = self::getAllChampions();
+
+        $freeChampions = [];
 
         foreach ($champions as $champion) {
             if (in_array($champion['key'], $freeChampionsId['freeChampionIds'])) {
@@ -63,10 +89,13 @@ class LeagueOfLegendApi
         }
 
         return $freeChampions;
+
     }
 
-    public static function getSummoner(string $summonerName)
+
+    public static function getSummoner(string $summonerName = "")
     {
+        self::setUpApiKey();
         try {
             $summoner = self::callApi('https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/' . $summonerName . '?api_key=' . self::$apiKey)
                 ->toArray();
@@ -78,17 +107,12 @@ class LeagueOfLegendApi
         $summoner = self::callApi('https://euw1.api.riotgames.com/lol/league/v4/entries/by-summoner/' . $summoner['id'] . '?api_key=' . self::$apiKey)
             ->toArray();
 
+        if(empty($summoner)) return null;
+
         return $summoner;
 
     }
 
-    private static function setUpApiKey()
-    {
-        if (self::$apiKey == null && !empty($_ENV['API_KEY'])) {
-            self::$apiKey = $_ENV['API_KEY'];
-        } else {
-            throw new Exception('Error, you did not enter your Api Key in the env file');
-        }
-    }
+
 
 }
